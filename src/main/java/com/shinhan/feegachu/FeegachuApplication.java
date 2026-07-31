bc863433a7e5618ee8affd5c5e68b1b11ca87f8e
HyunJuneKang <halfkang67@naver.com>
2026-07-31 16:45:17 +0900

    first commit

---
+++ src/main/java/com/shinhan/feegachu/FeegachuApplication.java
@@ -0,0 +1,13 @@
+package com.shinhan.feegachu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeegachuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeegachuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shinhan/feegachu/FeegachuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinhan.feegachu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeegachuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
